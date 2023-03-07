97f5f8301c9e62b3a1a9df2732339795a97f2008
Franco <francosalvadeo1@gmail.com>
2023-03-07 04:41:12 -0300

    se agregan detalles para carrito

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,27 +1,33 @@
 import { createContext, useContext, useState } from 'react'
 
 export const CartContext = createContext([])
-
 export const useCartContext = () => useContext(CartContext)
-
 export const CartContextProvider = ({children}) => {
     const [ ListaCart, setListaCart] = useState([])
     const agregarACart = (nuevoVehiculo) => {
+        const idVehiculoCart = ListaCart.findIndex(vehiculo => vehiculo.id === nuevoVehiculo.id)
+        if(idVehiculoCart !== -1) {
+            ListaCart[idVehiculoCart].cantidad += nuevoVehiculo.cantidad
+            setListaCart([...ListaCart])
+        }
         setListaCart([
             ... ListaCart,
             nuevoVehiculo
         ])
     }
 
-    const total = () => ListaCart.reduce( (contador, vehiculo) => contador += vehiculo.cantidad, 0)
-
+    const totalPrecio = () => ListaCart.reduce( (contador, vehiculo) => contador += (vehiculo.cantidad * vehiculo.precio), 0)
+    const totalCantidad = () => ListaCart.reduce( (contador, vehiculo) => contador + vehiculo.cantidad, 0)
+    const eliminarVehiculo = (id) => setListaCart(ListaCart.filter(vehiculo => vehiculo.id !== id))
     const vaciar = () => setListaCart ([])
 
     return (
         <CartContext.Provider value={{
             ListaCart,
             agregarACart,
-            total,
+            totalPrecio,
+            totalCantidad,
+            eliminarVehiculo,
             vaciar
         }}>
             {children}
--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import { useCartContext } from '../../Context/CartContext'
 
 const CartContainer = () => {
-    const { ListaCart, vaciar } = useCartContext()
+    const { ListaCart, vaciar, totalPrecio, eliminarVehiculo } = useCartContext()
   return (
     <div>
-        { ListaCart.map(vehiculo => (
-            <div key={vehiculo.id}>
-                <img src={vehiculo.imagen} style={{width: 150, marginLeft: 10, marginTop: 10}}/>
+        { ListaCart.map(nuevoVehiculo => (
+            <div key={nuevoVehiculo.id}>
+                <img src={nuevoVehiculo.imagen} style={{width: 150, marginLeft: 10, marginTop: 10}}/>
                 <div style={{display: 'inline-block', marginLeft: 10, marginRight: 10}}>
-                   <div style={{display: 'inline-block', fontWeight: 'bold'}}>{vehiculo.nombre}</div> - Cantidad: {vehiculo.cantidad} - Precio: {vehiculo.precio} {vehiculo.moneda}
-                </div>              
+                   <div style={{display: 'inline-block', fontWeight: 'bold'}}>{nuevoVehiculo.nombre}</div> - Cantidad: {nuevoVehiculo.cantidad} - Precio: {nuevoVehiculo.precio * nuevoVehiculo.cantidad} {nuevoVehiculo.moneda}
+                </div>
+                <div>
+                  <button className="btn btn-danger" onClick={()=> eliminarVehiculo(nuevoVehiculo.id)}> Eliminar vehiculo</button>
+                </div>             
             </div>
         )
         )            
         }
+        <center>
+          <h4>{totalPrecio() !== 0 && `Precio Total es: $ ${totalPrecio()}`}</h4>
+        </center>
         <center>
           <button onClick={vaciar} className="btn btn-outline-danger" style={{margin: 20}}>Vaciar</button>                        
         </center>
